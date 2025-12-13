93d5eb208d51fb19b2b3b961824a81aa7b8f06ad
Yugdeep <yugdeepkoirala81@gmail.com>
2025-12-13 16:03:23 +0545

    AI.ML

---
+++ day1/num.py
@@ -0,0 +1,14 @@
+import numpy as np
+import ipykernel 
+import pandas
+import matplotlib
+import seaborn
+import jupyter
+
+ar_0=np.zeros((2,3), dtype=int)
+print(ar_0)
+ar_1=np.ones((3,2), dtype=int)
+ar_iden=np.eye(4)
+print(ar_1)
+print(ar_iden)
+print(ar_0.shape)
